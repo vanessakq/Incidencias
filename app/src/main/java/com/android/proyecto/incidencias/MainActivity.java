0588ca3fd8a0bedb72401be2da9cb4777a410f4a
Katherine Vanessa Quispe Gardella <vanessa.kq@gmail.com>
2015-11-22 21:50:48 -0500

    Commit 22/11/2015 9:50pm

--- app/src/main/java/com/android/proyecto/incidencias/MainActivity.java
+++ app/src/main/java/com/android/proyecto/incidencias/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(getApplicationContext(), "Credenciales incorrectas",Toast.LENGTH_SHORT).show();
 
 
-                /*Katherine Vanessa Quispe Gardella soy un mono*/
+                /*Katherine Vanessa Quispe Gardella soy un monooooooooooooooooooo*/
 
                 /*Aqui esta mi cambio mono*/
             }
