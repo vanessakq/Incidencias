2b2156151b8b3b701bd42bb37cc6d1a1b017634f
Katherine Vanessa Quispe Gardella <vanessa.kq@gmail.com>
2015-11-30 14:16:18 -0500

    Commit Vanessa 30/11/2015 - 02:16

---
+++ app/src/main/java/com/android/proyecto/incidencias/RegistroIncidenciaActivity.java
@@ -0,0 +1,15 @@
+package com.android.proyecto.incidencias;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kquispe on 30/11/2015.
+ */
+public class RegistroIncidenciaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_incidencia);
+    }
+}
